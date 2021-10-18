b35bfd95ecc22fb7d2982a4f3d31043716fd3b1b
GastonNHaczek <ghaczek@alumno.unlam.edu.ar>
2021-10-17 22:55:56 -0300

    login completo (falta registrar)

---
+++ helpers/MyDatabase.php
@@ -0,0 +1,34 @@
+<?php
+
+class MyDatabase{
+    private $connection;
+
+    public function __construct($servername, $username, $password, $dbname){
+        $this->connection = mysqli_connect($servername, $username,$password, $dbname);
+
+        if (!$this->connection) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+    }
+
+    public function __destruct(){
+        mysqli_close($this->connection);
+    }
+
+    public function query($sql){
+
+        $databaseResult = mysqli_query($this->connection, $sql);
+       /*
+        if($databaseResult){
+            echo "ENTRE A CONSULTAR";
+        }else{
+            echo "cagamos";
+        }
+*/
+        if (mysqli_num_rows($databaseResult) <= 0)
+            return [];
+
+        return mysqli_fetch_all($databaseResult,MYSQLI_ASSOC);
+
+    }
+}
