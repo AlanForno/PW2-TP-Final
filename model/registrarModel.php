a859124949f7bed649619bd95b1859bcc714ca2b
IanWagnerLautaro <ianwagnerlautaro@gmail.com>
2021-10-30 23:12:46 -0300

    Junte turnos y sesiones

--- model/registrarModel.php
+++ model/registrarModel.php
@@ -3,30 +3,51 @@
 
 class RegistrarModel
 {
-        private $database;
+
+
+    private $database;
+    private $resultado;
 
     public function __construct($database){
         $this->database = $database;
     }
 
-    public function registrarUsuario($usuario,$password,$email){
-        $validacion=md5(time());
+    public function registrarUsuario($usuario,$password, $rol, $email, $validacion){
+
         $md5password=md5($password);
-       // $mensaje= "hola!" . $usuario . "este es su codigo de confirmacion" . $validacion;
-       // echo "enviado";
-      //  mail($email,"correo de confirmacion",$mensaje);
-        $confirmacion=1;
-        $sql="INSERT INTO `usuario` (`usuario`, `password`, `validacion`,`confirmado` ) VALUES ('".$usuario."', '".$md5password."', '".$validacion."','".$confirmacion."')";
+        $sql="INSERT INTO `pw2`.`usuario` (`usuario`, `password`, `validacion`, `rol`, `email`  ) VALUES ('".$usuario."', '".$md5password."', '".$validacion."', '".$rol."' , '".$email."')";
         $this->database->insert($sql);
+
+
     }
-    /*
-    public function confirmacion($validacion){
-        $sql="SELECT * from 'usuario' where validacion='$validacion'";
-        if($this->database->query($sql)){
-            $sql1="update confirmado set 1 where validacion='$validacion'";
-            $this->database->update($sql);
+
+    public function validarUsuario($validacion, $email){
+
+        $sql="SELECT * FROM usuario";
+        $this->resultado=$this->database->query($sql);
+        if($this->verificar($email,$validacion,$this->resultado)){
+            $this->validacionCorrecta($email);
+        }else{
+            // AQUI DEBERIA ENVIAR UN MENSAJE DE ERROR
         }
     }
-*/
-    /* DEMAS METODOS DE REGISTRAR*/
+
+    public function verificar($email,$validacion,$resultado){
+        foreach ($resultado as $primerUsuario) {
+            $emailAComparar=$primerUsuario['email'];
+            $validacionAComparar=$primerUsuario['validacion'];
+
+            if (strcmp($email, $emailAComparar) == 0 && strcmp($validacion, $validacionAComparar) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function validacionCorrecta($email)
+    {
+        $sql="UPDATE `pw2`.`usuario` SET `validacion` = NULL WHERE (`email` = '".$email."')";
+        $this->database->insert($sql);
+    }
+
 }
