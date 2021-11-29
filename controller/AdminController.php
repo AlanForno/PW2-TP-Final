8a5a8fd9ec5e431cf030e5f93eea6a95dcd54fd9
juanmgarciapena <juanmpenagarcia@gmail.com>
2021-11-29 11:49:55 -0300

    reportes update

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -21,12 +21,23 @@ class AdminController{
 
     public function reportes(){
         $data= $this->sesion->obtenerPermisos();
-
+        /**Busqueda cantidad cavinas reservadas */
         $reservasCabinas=$this->vuelosModel->obtenerCabinasReservadas();
-
         $data["cabinaSuiteReservadas"]=$reservasCabinas["suite"];
         $data["cabinaFamiliarReservadas"]=$reservasCabinas["familiar"];
         $data["cabinaGeneralReservadas"]=$reservasCabinas["general"];
+
+        /**Busqueda tipos de aeronave */
+        $cantTipoDeVuelos = $this->vuelosModel->obtenerTiposAeronave();
+        $data["tipo1"]=$cantTipoDeVuelos["tipo1"];
+        $data["tipo2"]=$cantTipoDeVuelos["tipo2"];
+        $data["tipo3"]=$cantTipoDeVuelos["tipo3"];
+        
+        /**Busqueda turnos dados por hospital */
+        $cantTurnosHospitales = $this->vuelosModel->obtenerTurnosHospitales();
+        $data["bsas"]=$cantTurnosHospitales["bsas"];
+        $data["shangai"]=$cantTurnosHospitales["shangai"];
+        $data["ankara"]=$cantTurnosHospitales["ankara"];
 
         echo $this->printer->render( "view/reportes.html", $data);
     }
